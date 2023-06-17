253ba9772e367eb79ec11542bc8dc3f023f0ac69
abgox <abgohxf@outlook.com>
2023-06-17 17:02:46 +0800

    Update volta-tab-completion : version(1.1.0)

--- volta-tab-completion/volta-tab-completion.psd1
+++ volta-tab-completion/volta-tab-completion.psd1
@@ -8,43 +8,30 @@
 
 @{
 
-    # Script module or binary module file associated with this manifest.
     RootModule    = 'volta-tab-completion.psm1'
 
-    # Version number of this module.
-    ModuleVersion = '1.0.2'
+    ModuleVersion = '1.1.0'
 
-    # ID used to uniquely identify this module
     GUID          = 'fc4639d8-7bc8-41da-ae8f-d38fc4b90890'
 
-    # Author of this module
     Author        = 'abgo'
 
-    # Company or vendor of this module
-    CompanyName   = 'Unknown'
-
-    # Copyright statement for this module
     Copyright     = '(c) abgo. All rights reserved.'
 
-    # Description of the functionality provided by this module
-    Description   = 'volta tab completion'
+    Description   = 'Volta tab completion.Support DIY(do-it-yourself) by editing JSON. For more information, please visit the project: https://github.com/abgox/PS-completions'
 
-    # Private data to pass to the module specified in RootModule/ModuleToProcess. This may also contain a PSData hashtable with additional module metadata used by PowerShell.
     PrivateData   = @{
 
         PSData = @{
 
-            # Tags applied to this module. These help with module discovery in online galleries.
-            Tags       = @('volta', 'tab', 'completion','tab-comletion')
+            Tags       = @('volta', 'tab', 'completion', 'tab-comletion', 'Support DIY(do-it-yourself)')
 
-            # A URL to the license for this module.
             LicenseUri = 'https://github.com/abgox/PS-completions/blob/main/LICENSE'
 
-            # A URL to the main website for this project.
             ProjectUri = 'https://github.com/abgox/PS-completions'
 
-        } # End of PSData hashtable
+        }
 
-    }# End of PrivateData hashtable
+    }
 }
 
--- volta-tab-completion/volta-tab-completion.psm1
+++ volta-tab-completion/volta-tab-completion.psm1
@@ -1,83 +1,40 @@
 <#
 # @Author      : abgox
 # @Github      : https://github.com/abgox/PS-completions
-# @description : volta tab completion script
 #>
 using namespace System.Management.Automation
 using namespace System.Management.Automation.Language
-Register-ArgumentCompleter -CommandName 'volta' -ScriptBlock {
+Register-ArgumentCompleter -CommandName ($(Split-Path -Path $PSScriptRoot -Leaf) -split '-')[0] -ScriptBlock {
     param($wordToComplete, $commandAst)
+
+    # Generate an ordered array
     $completions = [System.Collections.Specialized.OrderedDictionary]::new()
-    $tab_cmd = @('volta', 'pin', 'list', 'install', 'fetch', 'uninstall', 'which', 'run', 'setup', 'completions', 'help')
-    foreach ($_ in $tab_cmd) {
-        $completions[$_] = [System.Collections.Specialized.OrderedDictionary]::new()
+
+    #region : Parse json data
+    $json_file_name = (Split-Path -Path $PSScriptRoot -Leaf) + ".json"
+    $jsonContent = Get-Content -Raw -Path ($PSScriptRoot + "\" + $json_file_name) -Encoding UTF8 | ConvertFrom-Json
+    #endregion
+
+    #region : Store all tab-completion
+    foreach ($_ in $jsonContent.PSObject.Properties) {
+        $cmds = $_.Name
+        $help = $_.Value
+        $cmd = $cmds.substring(0, $cmds.lastIndexOf(' '))
+        $subCmd = $cmds.substring($cmds.lastIndexOf(' ') + 1)
+        $completions[$cmds] = [CompletionResult]::new($subcmd, $subcmd, 'ParameterValue', $help)
     }
-    function addTab($cmd, $subcmd, $Tip) {
-        $completions[$cmd] += @{$subcmd = [CompletionResult]::new($subcmd, $subcmd, 'ParameterValue', $Tip) }
-    }
-    addTab 'volta' 'pin' "Pin your project's runtime or package manager`neg: volta pin node@lts"
+    #endregion
 
-    addTab 'volta' 'list' "Display the current toolchain"
-    addTab 'list' '-c' "--current`nShow the currently-active tool(s)"
-    addTab 'list' '-d' "--default`nShow your default tool(s)"
-    addTab 'list' '--format' "Specify the output format [possible values: human, plain]`neg: volta list --format human`n    volta list --format plain"
-
-    addTab 'volta' 'install' "Install a tool in your toolchain`neg: volta install yarn@1"
-    addTab 'install' 'node' "Install node `neg: volta install node@lts`n    volta install node@16"
-    addTab 'install' 'npm' "Install npm`neg: volta install npm@8"
-    addTab 'install' 'yarn' "Install yarn`neg: volta install yarn@1"
-
-    addTab 'volta' 'fetch' "Fetch a tool to the local machine`neg: volta fetch yarn@1"
-    addTab 'fetch' 'node' "Fetch node `neg: volta fetch node@lts`n    volta fetch node@16"
-    addTab 'fetch' 'npm' "Fetch npm`neg: volta fetch npm@8"
-    addTab 'fetch' 'yarn' "Fetch yarn`neg: volta fetch yarn@1"
-
-    addTab 'volta' 'uninstall' "Uninstall a tool in your toolchain`neg: volta uninstall yarn@1"
-    addTab 'uninstall' 'node' "Uninstall node `neg: volta uninstall node@lts`n    volta uninstall node@16"
-    addTab 'uninstall' 'npm' "Uninstall npm`neg: volta uninstall npm@8"
-    addTab 'uninstall' 'yarn' "Uninstall yarn`neg: volta uninstall yarn@1"
-
-    addTab 'volta' 'which' "Locate the actual binary that will be called by Volta"
-    addTab 'which' 'node' "Locate the binary file for node"
-    addTab 'which' 'npm' "Locate the binary file for npm"
-    addTab 'which' 'yarn' "Locate the binary file for yarn"
-
-    addTab 'volta' 'run' "Run a command with custom Node, npm, pnpm, and/or Yarn versions"
-    addTab 'run' '--node' "Set the custom Node version`neg: volta run --node <version> <command>"
-    addTab 'run' '--npm' "Set the custom Npm version`neg: volta run --npm <version> <command>"
-    addTab 'run' '--yarn' "Set the custom Yarn version`neg: volta run --yarn <version> <command>"
-    addTab 'run' '--env' "Set an environment variable (can be used multiple times)`neg: volta run --env <NAME=value>"
-    addTab 'run' '--no-yarn' "Disable Yarn"
-    addTab 'run' '--bundled-npm' "Force npm to be the version bundled with Node"
-
-    addTab 'volta' 'setup' "Enable Volta for the current user / shell"
-
-    addTab 'volta' 'completions' "Generate Volta completions`nYou already have completions. You don't need it"
-
-    addTab 'volta' '-v' "--version`nPrint the current version of Volta"
-    addTab 'volta' 'help' "Show official help"
-    foreach ($_ in $tab_cmd) {
-        if ($_ -ne 'help') {
-            if ($_ -ne 'volta') {
-                addTab 'help' $_ "Show official help about the command -- volta $_"
-            }
-            addTab $_ '-h' "--help`nShow official help"
-            addTab $_ '--verbose' "Enables verbose diagnostics"
-            addTab $_ '--c' "Prevents unnecessary output"
-
-        }
-    }
-    # ---------------------------------
+    #region : Carry out
     $commandElements = $commandAst.CommandElements
-    $lastCommandElement = $commandElements[$commandElements.Count - 1].Value
-    foreach ($cmd in $completions.keys) {
-        if ($commandElements[$commandElements.Count - 2].Value -eq $cmd) {
-            $completions[$cmd].keys | Where-Object { $_ -like "$lastCommandElement*" } | ForEach-Object {
-                if ($lastCommandElement -ne $_ ) { $completions[$cmd][$_] }
-            }
+    function completion($num) {
+        # Space($num=0)/input($num=-1) and then tab
+        $completions.Keys | Where-Object { $_ -like "$commandElements*" } | ForEach-Object {
+            $input_space_count = ($commandElements -split ' ').Count - 1
+            $cmd_space_count = ($_ -split ' ').Count - 1
+            if ($input_space_count -eq $cmd_space_count + $num) { $completions[$_] }
         }
     }
-    $completions[$lastCommandElement].keys | Where-Object {
-        $_ -like "$wordToComplete*"
-    } | ForEach-Object { $completions[$lastCommandElement][$_] }
+    completion $(if ($wordToComplete.length) { 0 }else { -1 })
+    #endregion
 }
