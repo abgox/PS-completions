3bdf5611a76849c8db77c99204c7abcf6bf26359
abgox <abgohxf@outlook.com>
2023-06-19 00:05:59 +0800

    Update chfs-tab-completion

--- chfs-tab-completion/chfs-tab-completion.psd1
+++ chfs-tab-completion/chfs-tab-completion.psd1
@@ -7,25 +7,31 @@
 #
 
 @{
-    # Script module or binary module file associated with this manifest.
+
     RootModule    = 'chfs-tab-completion.psm1'
 
-    # Version number of this module.
-    ModuleVersion = '1.0.0'
+    ModuleVersion = '1.2.2'
 
-    # ID used to uniquely identify thxis module
     GUID          = '9b970e32-477e-4e78-9d0b-bf2802d3f4ba'
 
-    # Author of this module
     Author        = 'abgo'
 
-    # Company or vendor of this module
-    CompanyName   = 'Unknown'
-
-    # Copyright statement for this module
     Copyright     = '(c) abgo. All rights reserved.'
 
-    # Description of the functionality provided by this module
-    Description   = 'chfs tab completion'
+    Description   = 'chfs(CuteHttpFileServer) tab completion.Support-DIY(do-it-yourself) by editing JSON. For more information, please visit the project: https://github.com/abgox/PS-completions'
+
+    PrivateData   = @{
+
+        PSData = @{
+
+            Tags       = @('chfs', 'tab', 'completion', 'tab-comletion', 'Support-DIY(do-it-yourself)')
+
+            LicenseUri = 'https://github.com/abgox/PS-completions/blob/main/LICENSE'
+
+            ProjectUri = 'https://github.com/abgox/PS-completions'
+
+        }
+
+    }
 }
 
--- chfs-tab-completion/chfs-tab-completion.psm1
+++ chfs-tab-completion/chfs-tab-completion.psm1
@@ -1,46 +1,40 @@
 <#
 # @Author      : abgox
 # @Github      : https://github.com/abgox/PS-completions
-# @description : chfs tab completion script
 #>
 using namespace System.Management.Automation
 using namespace System.Management.Automation.Language
-Register-ArgumentCompleter -CommandName 'chfs' -ScriptBlock {
+Register-ArgumentCompleter -CommandName ($(Split-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -Leaf) -split '-')[0] -ScriptBlock {
     param($wordToComplete, $commandAst)
+
+    # Generate an ordered array
     $completions = [System.Collections.Specialized.OrderedDictionary]::new()
-    $tab_cmd = @('chfs')
-    foreach ($_ in $tab_cmd) {
-        $completions[$_] = [System.Collections.Specialized.OrderedDictionary]::new()
+
+    #region : Parse json data
+    $json_file_name = (Split-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -Leaf) + ".json"
+    $jsonContent = Get-Content -Raw -Path ($PSScriptRoot + "\" + $json_file_name) -Encoding UTF8 | ConvertFrom-Json
+    #endregion
+
+    #region : Store all tab-completion
+    foreach ($_ in $jsonContent.PSObject.Properties) {
+        $cmds = $_.Name
+        $help = $_.Value
+        $cmd = $cmds.substring(0, $cmds.lastIndexOf(' '))
+        $subCmd = $cmds.substring($cmds.lastIndexOf(' ') + 1)
+        $completions[$cmds] = [CompletionResult]::new($subcmd, $subcmd, 'ParameterValue', $help)
     }
-    function addTab($cmd, $subcmd, $Tip) {
-        $completions[$cmd] += @{$subcmd = [CompletionResult]::new($subcmd, $subcmd, 'ParameterValue', $Tip) }
-    }
-    addTab 'chfs' '--path=' "Directories where store shared files`neg: chfs --path=D:\`n    chfs --path=`"C:\Program Files`""
-    addTab 'chfs' '--port=' "HTTP listening port(Default is 80)`neg: chfs --port=8080"
+    #endregion
 
-    addTab 'chfs' '--log=' "Log directory. Empty value will disable log"
-
-    addTab 'chfs' '--version' "Show application version"
-
-    addTab 'chfs' '--allow=' "Allowed IPv4 addresses(Allow any address by default)`n White list mode: `"listitem1[,listitem2,...]`"`n More help info by running `"chfs --help`""
-
-    addTab 'chfs' '--rule=' "Access rules(anybody can access any thing by default).`nList defines like:`"USER:PWD:MASK[:DIR:MASK...][|...]`":`n`"192.168.1.2-192.168.1.10,192.169.1.222`" allows this 10 addresses.`nBlack list mode: `"not(listitem1[,listitem2,...])`"`n`"not(192.168.1.2-192.168.1.10,192.169.1.222)`" bans this 10 addresses!"
-
-    addTab 'chfs' '--file=' "A configuration file which overwrites & enhence the
-    settings"
-
-    addTab 'chfs' '--help' "Show context-sensitive help (also try --help-long and  --help-man)"
-    # ---------------------------------
+    #region : Carry out
     $commandElements = $commandAst.CommandElements
-    $lastCommandElement = $commandElements[$commandElements.Count - 1].Value
-    foreach ($cmd in $completions.keys) {
-        if ($commandElements[$commandElements.Count - 2].Value -eq $cmd) {
-            $completions[$cmd].keys | Where-Object { $_ -like "$lastCommandElement*" } | ForEach-Object {
-                if ($lastCommandElement -ne $_ ) { $completions[$cmd][$_] }
-            }
+    function completion($num) {
+        # Space($num=0)/input($num=-1) and then tab
+        $completions.Keys | Where-Object { $_ -like "$commandElements*" } | ForEach-Object {
+            $input_space_count = ($commandElements -split ' ').Count - 1
+            $cmd_space_count = ($_ -split ' ').Count - 1
+            if ($input_space_count -eq $cmd_space_count + $num) { $completions[$_] }
         }
     }
-    $completions[$lastCommandElement].keys | Where-Object {
-        $_ -like "$wordToComplete*"
-    } | ForEach-Object { $completions[$lastCommandElement][$_] }
+    completion $(if ($wordToComplete.length) { 0 }else { -1 })
+    #endregion
 }
